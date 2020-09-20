7a985a2790f25e679bbd4de4556387d4c00b07b1
Alvin Mantovani <alvin.ictn@gmail.com>
2020-09-20 19:37:52 +0700

    Updates

---
+++ precache-manifest.f98cdbc1523a1c5059aa2b17c90adfdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f874b1834294a5d5d52ffe5535d46cf0",
+    "url": "/Medium-Times-Clones/index.html"
+  },
+  {
+    "revision": "6fddc37f32b726bea736",
+    "url": "/Medium-Times-Clones/static/css/main.68ba0d78.chunk.css"
+  },
+  {
+    "revision": "4dea582e70d206ded9dd",
+    "url": "/Medium-Times-Clones/static/js/2.c0bfb1f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Medium-Times-Clones/static/js/2.c0bfb1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fddc37f32b726bea736",
+    "url": "/Medium-Times-Clones/static/js/main.8f595c59.chunk.js"
+  },
+  {
+    "revision": "88f26a69a123f84dc413",
+    "url": "/Medium-Times-Clones/static/js/runtime-main.38a7da0f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Medium-Times-Clones/static/media/logo.5d5d9eef.svg"
+  }
+]);
